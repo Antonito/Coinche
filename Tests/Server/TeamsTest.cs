5343d076f79886d3c61b3ac119f4baff438e4e68
Antoine Bache <antoine.bache@epitech.eu>
2017-11-06 16:21:45 +0100

    Added Team unit tests, fixed HasWon

---
+++ Tests/Server/TeamsTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Coinche.Server.Core;
+using NUnit.Framework;
+
+namespace Server
+{
+    [TestFixture]
+    public class Teams
+    {
+        [Test]
+        public void TeamInitialScore()
+        {
+            var team = new Team(new Player(), new Player());
+
+            Assert.AreEqual(0, team.Score);
+        }
+
+        [Test]
+        public void TeamAddScore()
+        {
+            var team = new Team(new Player(), new Player());
+
+            team.AddScore(200);
+            Assert.AreEqual(200, team.Score);
+        }
+
+        [Test]
+        public void TeamHasNotWonNoScore()
+        {
+            var team = new Team(new Player(), new Player());
+
+            Assert.AreEqual(false, team.HasWon());
+        }
+
+        [Test]
+        public void TeamHasNotWonSmallScore()
+        {
+            var team = new Team(new Player(), new Player());
+
+            team.AddScore(200);
+            Assert.AreEqual(false, team.HasWon());
+        }
+
+        [Test]
+        public void TeamHasWonEqualScore()
+        {
+            var team = new Team(new Player(), new Player());
+
+            team.AddScore(team.MaxScore);
+            Assert.AreEqual(true, team.HasWon());
+        }
+
+        [Test]
+        public void TeamHasWonBigScore()
+        {
+            var team = new Team(new Player(), new Player());
+
+            team.AddScore(team.MaxScore + 5000);
+            Assert.AreEqual(true, team.HasWon());
+        }
+    }
+}
